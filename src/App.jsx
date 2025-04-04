4b92228674fec9e5f608c9b519c1b5b7a6d607ff
Yohannes G.Simegne <ygetahun37@gmail.com>
2025-04-04 11:29:40 +0300

    parameter routing tested

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
-
+import City from "./components/City";
 const BASE_URL = "http://localhost:8000";
 
 export default function App() {
@@ -34,9 +34,9 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />}>
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />}>
           <Route
             index
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -45,13 +45,14 @@ export default function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
           <Route path="form" element={<p>Form</p>} />
         </Route>
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<Pagenotfound />} />
       </Routes>
     </BrowserRouter>
