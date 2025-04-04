c7b037c0d575ae8963b58671ce4dd62f09805ee1
Yohannes G.Simegne <ygetahun37@gmail.com>
2025-04-04 16:00:54 +0300

    programatically navigate implemented

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 const BASE_URL = "http://localhost:8000";
 
 export default function App() {
@@ -50,7 +51,7 @@ export default function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<Pagenotfound />} />
