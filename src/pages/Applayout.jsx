d8a5fe1c94851c3e7632a04acedacbe650d68c2a
Yohannes G.Simegne <ygetahun37@gmail.com>
2025-04-14 14:01:58 +0300

    Protected route implemented

--- src/pages/Applayout.jsx
+++ src/pages/Applayout.jsx
@@ -6,7 +6,6 @@ import User from "../components/User";
 export default function AppLayout() {
   return (
     <div className={styles.app}>
-      {/* <AppNav /> */}
       <SideBar />
       <Map />
       <User />
